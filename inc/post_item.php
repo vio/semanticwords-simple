c4b99dd1adc48ff2dcd7ff0fdce5f19c6b338621
Vio <viorel@beanon.com>
2008-10-22 15:19:53 +0300

    styles updated

--- inc/post_item.php
+++ inc/post_item.php
@@ -7,27 +7,28 @@
 			$_link	= get_permalink( $post->ID );
 			
 			?>
-			
-			<h2>
-				<a href="<?php echo $_link ?>" rel="bookmark" title="<?php _e('Read more about '); echo '\''.$_title.'\''?>"><?php echo $_title ?></a>
-			</h2>
+			<li>
+				<h2>
+					<a href="<?php echo $_link ?>" rel="bookmark" title="<?php _e('Read more about '); echo '\''.$_title.'\''?>"><?php echo $_title ?></a>
+				</h2>
 
-			<p><?php _e( 'Posted on' ) ?> <?php the_time('F jS, Y') ?> by <?php the_author() ?> </p>
+				<p class="date"><?php _e( 'Posted on' ) ?> <?php the_time('F jS, Y') ?> by <?php the_author() ?> </p>
 
-			<?php the_content('Read the rest of this entry &raquo;'); ?>
+				<?php the_content('Read the rest of this entry &raquo;'); ?>
 
-			<div class="metadata">
-				<h3>Post metadata</h3>
-				<ul>
-					<li>
-						<h4>Tags</h4>
-						<?php the_tags('Tags: ', ', ', '<br />'); ?>
-					</li>
-					<li>
-						<h4>Posted in</h4>
-						<?php the_category(', ') ?>
-					</li>
-					<li><?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></li>
-					<li><?php edit_post_link('Edit', '', ''); ?></li>
-				</ul>
-			</div>
+				<div class="details">
+					<h3>Post details</h3>
+					<ul>
+						<li>
+							<h4>Tags</h4>
+							<?php the_tags('', ', ', '<br />'); ?>
+						</li>
+						<li>
+							<h4>Posted in</h4>
+							<?php the_category(', ') ?>
+						</li>
+						<li><?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></li>
+						<li><?php edit_post_link('Edit', '', ''); ?></li>
+					</ul>
+				</div>
+			</li>
